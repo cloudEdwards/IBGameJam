67ed03afa9b8248c4a3508535771eebc4029b848
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2025-04-07 10:45:37 -0400

    Modifier Added

--- Assets/_Project/Script/AbilitySystem/Ability.cs
+++ Assets/_Project/Script/AbilitySystem/Ability.cs
@@ -1,8 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Project.Script.AbilitySystem
 {
+    public enum ModifierType
+    {
+        Additive,
+        Multiplicative
+    }
+    [Serializable]
+    public class Modifier
+    {
+        public float BaseValue;
+        public ModifierType ModifierType;
+
+        public bool HasDuration = false;
+        public float Duration = -1;
+        public float CreatedTime;
+
+        public Modifier(float baseValue, ModifierType modifierType)
+        {
+            BaseValue = baseValue;
+            ModifierType = modifierType;
+            HasDuration = false;
+            Duration = -1;
+            CreatedTime = Time.time;
+        }
+        
+        public Modifier(float baseValue, ModifierType modifierType, float duration)
+        {
+            BaseValue = baseValue;
+            ModifierType = modifierType;
+            HasDuration = true;
+            Duration = duration;
+            CreatedTime = Time.time;
+        }
+    }
+    
+    public struct kfloat
+    {
+        public float BaseValue;
+        public float Value => CalculateFinal();
+        
+        private List<Modifier> _modifiers;
+        
+        public kfloat(float baseValue)
+        {
+            BaseValue = baseValue;
+            _modifiers = new List<Modifier>();
+        }
+
+        public float CalculateFinal()
+        {
+            float additive = 0f;
+            float multiplicative = 1f;
+
+            for (int i = 0; i < _modifiers.Count - 1; i++)
+            {
+                var mod = _modifiers[i];
+
+                if (mod.HasDuration && Time.time >= mod.CreatedTime + mod.Duration)
+                {
+                    _modifiers.RemoveAt(i);
+                    continue;
+                }
+
+                if (mod.ModifierType == ModifierType.Additive)
+                    additive += mod.BaseValue;
+                else
+                    multiplicative *= mod.BaseValue;
+
+            }
+
+            float final = (BaseValue + additive) * multiplicative;
+            return final > 0 ? final : 0;
+        }
+        
+        public void AddModifier(Modifier newModifier)
+        {
+            _modifiers.Add(newModifier);
+        }
+
+        public void RemoveModifier(Modifier modifier)
+        {
+            if (_modifiers.Contains(modifier)) _modifiers.Remove(modifier);
+        }
+
+        public void CleanModifiers()
+        {
+            _modifiers.Clear();
+        }
+
+        public static implicit operator kfloat(float value) => new kfloat(value);
+        public static implicit operator float(kfloat kf) => kf.Value;
+    }
+    public class EntityStats
+    {
+        public kfloat Speed;
+        public kfloat MaxHealth;
+        public kfloat BaseDamage;
+        public kfloat BaseDefense;
+    }
+    
     public interface IAbility
     {
         public void OnButtonDown();
@@ -13,11 +113,11 @@ namespace _Project.Script.AbilitySystem
     [Flags]
     public enum AbilityTargeting
     {
-        None      = 0,
-        Self      = 1 << 0,
-        Friendly  = 1 << 1,
-        Hostile   = 1 << 2,
-        All       = ~(~0 << 4)
+        None     = 0,
+        Self     = 1 << 0,
+        Friendly = 1 << 1,
+        Hostile  = 1 << 2,
+        All      = ~(~0 << 4)
     }
     
     public abstract class Ability : MonoBehaviour, IAbility
@@ -30,15 +130,19 @@ namespace _Project.Script.AbilitySystem
         
         private float lastUsedTime = -999f;
         protected bool isFinished = false;
-        private bool isActive = false;
 
-        public bool IsActive => isActive;
+        public bool IsActive => enabled;
         public bool IsFinished => isFinished;
         public float RemainingTime => Mathf.Abs(Time.time - (lastUsedTime + Cooldown));
         public float RemainingTimeInverse => Mathf.InverseLerp(Cooldown, 0, RemainingTime);
         public virtual bool CanUse()
         {
             return Time.time >= lastUsedTime + Cooldown;
+        }
+
+        public void SetActivate(bool isActive)
+        {
+            enabled = isActive;
         }
 
         public void StartCooldown()
--- Assets/_Project/Script/AbilitySystem/AbilityManagerUI.cs
+++ Assets/_Project/Script/AbilitySystem/AbilityManagerUI.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Project.Script.AbilitySystem
 {
     public class AbilityManagerUI : MonoBehaviour
     {
+        
+        
+        [Header("Ability Settings")]
         public AbilityHolderUI PrimarySkill;
 
         [Header("Page 1")] 
@@ -20,8 +23,13 @@ namespace _Project.Script.AbilitySystem
         public AbilityHolderUI skill5;
         public AbilityHolderUI skill6;
 
-
+        // Private
         private Dictionary<int, AbilityHolderUI> indexer;
+        private Tween _switchSkillSetTween;
+
+        private Sequence _currentSequence;
+        private Sequence _page1Seq;
+        private Sequence _page2Seq;
 
         private void Start()
         {
@@ -33,12 +41,50 @@ namespace _Project.Script.AbilitySystem
             indexer.Add(4, skill4);
             indexer.Add(5, skill5);
             indexer.Add(6, skill6);
+
+            
+            float delay = 0.2f;
+            RectTransform page1Rect = page1Object.GetComponent<RectTransform>();
+            RectTransform page2Rect = page2Object.GetComponent<RectTransform>();
+            Vector3 startPos1 = page1Rect.anchoredPosition;
+            Vector3 startPos2 = page2Rect.anchoredPosition;
+
+            _page1Seq = DOTween.Sequence();
+            _page1Seq.Join(page1Object.GetComponent<CanvasGroup>().DOFade(1, delay));
+            _page1Seq.Join(page1Rect.DOAnchorPos(startPos1 + Vector3.up * 100, 0)); // Jump instantly up
+            _page1Seq.Join(page1Rect.DOAnchorPos(startPos1, delay));                 // Animate back to start
+            _page1Seq.Join(page2Object.GetComponent<CanvasGroup>().DOFade(0, delay));
+            _page1Seq.Join(page2Rect.DOAnchorPos(startPos2 - Vector3.up * 100, delay));
+            _page1Seq.SetAutoKill(false);
+            // _page1Seq.Pause();
+
+            _page2Seq = DOTween.Sequence();
+            _page2Seq.Join(page2Object.GetComponent<CanvasGroup>().DOFade(1, delay));
+            _page2Seq.Join(page2Rect.DOAnchorPos(startPos2 + Vector3.up * 100, 0));
+            _page2Seq.Join(page2Rect.DOAnchorPos(startPos2, delay));
+            _page2Seq.Join(page1Object.GetComponent<CanvasGroup>().DOFade(0, delay));
+            _page2Seq.Join(page1Rect.DOAnchorPos(startPos1 - Vector3.up * 100, delay));
+            _page2Seq.SetAutoKill(false);
+            _page2Seq.Pause();
         }
 
         private void Update()
         {
             TryUseAbility(KeyCode.Mouse0, PrimarySkill);
 
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                _currentSequence?.Complete();
+                _currentSequence = _page2Seq;
+                _currentSequence.Restart();
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                _currentSequence?.Complete();
+                _currentSequence = _page1Seq;
+                _currentSequence.Restart();
+            }
+            
             if (!Input.GetKey(KeyCode.LeftControl))
             {
                 TryUseAbility(KeyCode.Q, skill1);
@@ -47,6 +93,7 @@ namespace _Project.Script.AbilitySystem
             }
             else
             {
+                _switchSkillSetTween?.Kill();
                 TryUseAbility(KeyCode.Q, skill4);
                 TryUseAbility(KeyCode.E, skill5);
                 TryUseAbility(KeyCode.R, skill6);
@@ -57,6 +104,7 @@ namespace _Project.Script.AbilitySystem
         {
             Ability ability = skillUI.GetAbility;
             if (ability == null) return;
+            if (!ability.enabled) return;
             if (!ability.CanUse()) return;
             
             if (Input.GetKeyDown(keyCode))
